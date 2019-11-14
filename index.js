82b3c2fc274aab2e6a3d8894f3bb2711ce330cf5
Puja Fitzpatrick <pujafitz@gmail.com>
2019-11-14 15:14:26 -0500

    Magical things

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+require('dotenv');
+const axios = require('axios');
+
+app.use(express.static(path.join(__dirname, "public/")));
+
+app.use('/search/users', async (req, res) => {
+const { q } = req.query;
+const text = encodeURI(q);
+const {data} = await axios.get( `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+res.json(data);
+})
+
+app.use('/users/:username', async (req, res) => {
+let { username } = req.params;
+username = encodeURI(username);
+const {data} = await axios.get( https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+res.json(data);
+})
+
+app.listen(3300, ()=>console.log("Server is up on port 3300!"));
---
+++ public/precache-manifest.74eb6b40ccb659fdd3e6794897496553.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2f9f51f690580e1dbfe1f4dbeeed99e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cec18bbafdf08c918f1e",
+    "url": "/static/css/main.aafe1f20.chunk.css"
+  },
+  {
+    "revision": "a6323d0355bb20614605",
+    "url": "/static/js/2.9e6de456.chunk.js"
+  },
+  {
+    "revision": "cec18bbafdf08c918f1e",
+    "url": "/static/js/main.ebf2eaf7.chunk.js"
+  },
+  {
+    "revision": "c3acfd3c739c47148f9d",
+    "url": "/static/js/runtime-main.3cfc67bb.js"
+  }
+]);
